282fdc8904992cf71aab86d20e81ad7771c64d1c
RAJPUTRoCkStAr <sumitsingh9441@gmail.com>
2024-09-19 00:56:27 +0530

    changes

--- a,.py
+++
@@ -1,5 +0,0 @@
-for ticker in stocks:
-                stock = yf.Ticker(ticker)
-                stock_info = pd.DataFrame(stock.info.items(), columns=['Attribute', 'Value'])
-                st.write(f"### {ticker}")
-                st.dataframe(stock_info)
